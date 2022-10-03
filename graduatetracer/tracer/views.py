135140811f47d1b17991855bda32ed02c0fcb7a6
piedra123456 <rosepiedrasingco123@gmail.com>
2022-10-03 16:41:29 -0400

    okay

--- graduatetracer/tracer/views.py
+++ graduatetracer/tracer/views.py
@@ -663,10 +663,13 @@ class PostListView(LoginRequiredMixin, View):
         login_in_user = request.user
         posts = Post.objects.all().order_by('-created_on')
         form = PostForm()
+        grad_infos = User.objects.all
+        user = request.user
 
         context = {
             'post_list': posts,
             'form': form,
+            'grad_infos': grad_infos,
         }
 
         return render(request, 'tracer/user/post_list.html', context)
