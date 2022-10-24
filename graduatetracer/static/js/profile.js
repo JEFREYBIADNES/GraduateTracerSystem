7fbb85bf2805fbde0a596da9cb9290777fd43ad5
HuniHisoler <93319191+HuniHisoler@users.noreply.github.com>
2022-10-24 16:21:07 +0800

    adjust

--- graduatetracer/static/js/profile.js
+++ graduatetracer/static/js/profile.js
@@ -37,12 +37,18 @@ file.addEventListener('change', function() {
        
     }
  });
-
  function myFunction() {
-    var x = document.getElementById("i-box");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-//   }
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("btn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
